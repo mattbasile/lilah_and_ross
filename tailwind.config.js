18c12ff04ece1b540110d67210da9db4454f6c4e
mattbasile <matthewbasile2@gmail.com>
2023-05-12 14:27:05 -0400

    lilah and ross

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        brick: '#8C5430',
+        aqua: '#7A9478',
+        yellow: '#f5efc0',
+      },
+      backgroundImage: {
+        'hero-pattern':
+          "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('../images/Flowers-Header.png')",
+        'travel-pattern':
+          "linear-gradient(rgba(122, 148, 120, 0.4), rgba(122, 148, 120, 0.4)), url('../images/prouts-neck.jpeg')",
+        olive:
+          "linear-gradient(rgba(0, 0, 0, 0.1), rgba(0, 0, 0, 0.1)), url('../images/Olive.jpeg')",
+      },
+    },
+    fontFamily: {
+      header: ['Cyriele', ...defaultTheme.fontFamily.sans],
+      body: ['rousseau_decobold', ...defaultTheme.fontFamily.serif],
+    },
+  },
+  plugins: [],
+};
